b003b49b43b79e1ea502b07e107b05cfd5df3440
Yuding Duan <danbzbr@gmail.com>
2025-06-15 15:24:00 +0200

    visit

--- src/crew_fashion_color/crew.py
+++ src/crew_fashion_color/crew.py
@@ -4,17 +4,42 @@ from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from crewai_tools import FirecrawlCrawlWebsiteTool, tool
 
 from crewai_tools import SerperDevTool, FirecrawlSearchTool, ScrapeWebsiteTool
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
+# read the photo
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+Image_path = input('enter the path of your original design:\n')
+
+@tool("read the content of original design")
+def read_image_content(image_path: str) -> str:
+    """
+    Reads the content of an image file and returns a description.
+    """
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+    raw_image = Image.open(image_path).convert('RGB')
+    # unconditional image captioning
+    inputs = processor(raw_image, return_tensors="pt")
+    out = model.generate(**inputs)
+
+    return processor.decode(out[0], skip_special_tokens=True)
+
+
 google_search = SerperDevTool(n_results=10, country='fr')
+fire_tool = FirecrawlCrawlWebsiteTool(url='firecrawl.dev')
 
 reasoner = llm = LLM(
     model="anthropic/claude-opus-4-20250514"
 )
+
+
 
 
 
@@ -37,7 +62,7 @@ class CrewFashionColor():
             config=self.agents_config['researcher'], # type: ignore[index]
             verbose=True,
             tools=[
-                google_search
+                read_image_content,google_search, fire_tool
             ]
         )
 
